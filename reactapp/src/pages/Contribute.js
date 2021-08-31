07365ba54a0d47d05ed98147d29344866102d824
ademcskn <ademcskn@hotmail.com>
2021-08-31 11:54:41 +0300

    Düzenleme gerçekleşti

--- reactapp/src/pages/Contribute.js
+++ reactapp/src/pages/Contribute.js
@@ -5,7 +5,7 @@ function Contribute() {
         <div>
             <h3>Project Files and Contrübutes to Project</h3>
             <p>
-                You can dowload full project from <a target="_blank" rel="noreferrer"  href="https://github.com/ademcskn">this</a> github page
+                You can dowload full project from <a target="_blank" rel="noreferrer"  href="https://github.com/ademcskn/ReactTutorial">this</a> github page
                 also, you can contribute to this project.
             </p>
         </div>
